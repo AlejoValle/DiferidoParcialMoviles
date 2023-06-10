378a6e9f463ea125ce350d0654c368a1c9d6339d
A <00139121@uca.edu.sv>
2023-06-10 10:46:58 -0500

    capa adaptador

--- Diferido/app/src/main/java/com/ramirez/diferido/adapter/ProvinciaAdapter.kt
+++ Diferido/app/src/main/java/com/ramirez/diferido/adapter/ProvinciaAdapter.kt
@@ -4,29 +4,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ramirez.diferido.data.provincia
 
-class ProvinciaAdapter (
-    private val Click:(provincia)-> Unit
-): RecyclerView.Adapter<ProvinciaAdapter.ViewholderProvincia>(){
+class ProvinciaAdapter {
 
-    private val provincia:List<provincia> ?=null
 
- class ViewholderProvincia(private val binding: ItemProvinciaBinding) : RecyclerView.ViewHolder(binding.root){
-
-     fun bind(provincias:provincia, Click:(provincia) -> Unit){
-
-     }
-
- }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewholderProvincia {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewholderProvincia, position: Int) {
-        TODO("Not yet implemented")
-    }
 }
